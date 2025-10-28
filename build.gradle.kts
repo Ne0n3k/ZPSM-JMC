cb3524fb79f042c6a9a98980b1daa4db7d42ba63
Jakub <jakub.blazowski@gmail.com>
2025-10-28 13:52:58 +0100

    chore: init Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("com.android.library") version "8.5.2" apply false
+    kotlin("android") version "2.0.20" apply false
+    kotlin("multiplatform") version "2.0.20" apply false
+    id("org.jetbrains.compose") version "1.7.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
+}
+
+allprojects {
+
+}
