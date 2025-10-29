053567f68ed23f77628a280721928e37af39b67c
Jakub <jakub.blazowski@gmail.com>
2025-10-29 12:12:33 +0100

    fix: unstable API suspression

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,7 @@ pluginManagement {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
